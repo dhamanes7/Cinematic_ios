7c494c12d208e60f10ec33a5f59dda6ae3ebf8c6
shubhamdhamane <A00257743@mycambrian.ca>
2022-12-16 01:17:54 -0500

    Bug fixes and Search movie model added

---
+++ Cinematic/Controller/ShowMovieViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ShowMovieViewController.swift
+//  Cinematic
+//
+//  Created by Shubham Dhamane on 16/12/22.
+//
+
+import Foundation
---
+++ Models/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  Cinematic
+//
+//  Created by Shubham Dhamane on 16/12/22.
+//
+
+import Foundation
+
+struct Genre: Codable {
+    var id: Int = 0
+    var name: String = ""
+}
+
